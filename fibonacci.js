7ab205a4b5a86872e2fa3d30b5e4016c4cd4a0c1
Jonathan Graziani <jpgraziani@gmail.com>
2021-02-15 20:11:48 -0700

    fibonaci

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+/*
+Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+*/
+
+
+
+
+/*
+
+1. what is the input to the function
+7
+2. what is the output of the function 
+1, 1, 2, 3, 5, 8, 13
+
+*/
